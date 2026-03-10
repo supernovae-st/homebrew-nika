fa27eb163d3942c15aeb1a0c0ddf6b95386d253c
Thibaut MÉLEN <20891897+ThibautMelen@users.noreply.github.com>
2026-03-10 12:20:53 +0100

    chore: bump nika to v0.23.1

--- Formula/nika.rb
+++ Formula/nika.rb
@@ -1,30 +1,30 @@
 # Formula/nika.rb
-# v0.23.0: Comprehensive Audit Release
+# v0.23.1: Provider Definitions Fix
 class Nika < Formula
   desc "Semantic YAML workflow engine for AI"
   homepage "https://github.com/supernovae-st/nika"
-  version "0.23.0"
+  version "0.23.1"
   license "AGPL-3.0-or-later"
 
   on_macos do
     on_arm do
-      url "https://github.com/supernovae-st/nika/releases/download/v0.23.0/nika-macos-arm64-0.23.0.tar.gz"
-      sha256 "efd29d25e622f8a1826e2f51351e20b7ecd4de19f5924b1ae03a1614baa808e1"
+      url "https://github.com/supernovae-st/nika/releases/download/v0.23.1/nika-macos-arm64-0.23.1.tar.gz"
+      sha256 "6997a1f00a40d883ebf3c2648f083fbfd58e54fbed3bcf6a1ded2f8fc10c33bb"
     end
     on_intel do
-      url "https://github.com/supernovae-st/nika/releases/download/v0.23.0/nika-macos-x64-0.23.0.tar.gz"
-      sha256 "bf84047a1d50f3499193e12b8e45dc8e1f13624769dda134b9d5562e97f8b70e"
+      url "https://github.com/supernovae-st/nika/releases/download/v0.23.1/nika-macos-x64-0.23.1.tar.gz"
+      sha256 "6a4aaf50fc73b7c1b58d0d2e452fcf9e4eff481214994f01a8eeb9123d4d4571"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/supernovae-st/nika/releases/download/v0.23.0/nika-linux-arm64-0.23.0.tar.gz"
-      sha256 "163a1343eca50d8e16dfb7008759a93a182b41aef13b755f0142a078c0fdca57"
+      url "https://github.com/supernovae-st/nika/releases/download/v0.23.1/nika-linux-arm64-0.23.1.tar.gz"
+      sha256 "0000cb487db9f7128af31a598466629fbc0fb5210d4c0a628480a1417357fb73"
     end
     on_intel do
-      url "https://github.com/supernovae-st/nika/releases/download/v0.23.0/nika-linux-x64-0.23.0.tar.gz"
-      sha256 "2c8a8ac3a457b72852e84b6b48e70d3b5a7fa4c97f71fcb647455bcd0159e7d5"
+      url "https://github.com/supernovae-st/nika/releases/download/v0.23.1/nika-linux-x64-0.23.1.tar.gz"
+      sha256 "9f77face4d4f397e108e7a75684d784988e9b5eb214bf8d03732f58665e36343"
     end
   end
 
